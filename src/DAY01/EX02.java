aeb7f6a8edf767bd37cf4dc37a00f42fcf7fa1f5
김레생 <85009117+kimrasng@users.noreply.github.com>
2023-07-09 09:48:54 +0900

    wq

---
+++ src/DAY01/EX02.java
@@ -0,0 +1,31 @@
+package DAY01;
+
+public class EX02 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        // 문자열 출력
+        System.out.println('s');
+        System.out.println("String");
+
+        // 숫자 출력
+        System.out.println(10);
+        System.out.println(20);
+
+        // 줄 바꿈
+        System.out.println();
+
+        // 숫자 연산 결과 출력
+        System.out.println(1+10);
+        System.out.println(2+20);
+        System.out.println(3+30);
+
+        // 하나만 출력하기
+        System.out.println("Hello, JAVA!");
+
+        // 여러개 출력하기
+        System.out.println("Hello, JAVA!" + 9999 + "JAVA");
+
+    }
+
+}
