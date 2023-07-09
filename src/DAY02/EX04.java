a8bd2bcb9d3744dff738e65e82c7242ea0586bf5
김레생 <85009117+kimrasng@users.noreply.github.com>
2023-07-09 09:55:39 +0900

    test

---
+++ src/DAY02/EX04.java
@@ -0,0 +1,12 @@
+package DAY02;
+
+public class EX04 {
+    public static void main(String[] args){
+        // 논리 자료형
+        System.out.println(true);
+        System.out.println(false);
+
+        System.out.println(10 > 3);
+        System.out.println(11 * 11 < 10 * 12);
+    }
+}
